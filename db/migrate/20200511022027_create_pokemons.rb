4fcef77f423c5b20e66a2e74c0795f6c2cc79a88
Andrew Smith <848smith@gmail.com>
2020-05-11 02:26:26 +0000

    Build out tables for activerecord to have a database

---
+++ db/migrate/20200511022027_create_pokemons.rb
@@ -0,0 +1,15 @@
+class CreatePokemons < ActiveRecord::Migration[6.0]
+  def change
+    create_table :pokemons do |t|
+      t.string :name
+      t.string :type
+      t.integer :cp
+      t.integer :user_id
+      t.string :fast_move
+      t.string :power_move
+      t.integer :attack
+      t.integer :defense
+      t.integer :hp
+    end
+  end
+end
