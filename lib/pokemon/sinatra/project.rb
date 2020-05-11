5d4b96616f638d11bcbee70c9b3e173c6e2b6cf7
Andrew Smith <848smith@gmail.com>
2020-05-11 00:18:23 +0000

    first commit

---
+++ lib/pokemon/sinatra/project.rb
@@ -0,0 +1,10 @@
+require "pokemon/sinatra/project/version"
+
+module Pokemon
+  module Sinatra
+    module Project
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/pokemon/sinatra/project/version.rb
@@ -0,0 +1,7 @@
+module Pokemon
+  module Sinatra
+    module Project
+      VERSION = "0.1.0"
+    end
+  end
+end
