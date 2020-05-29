fe523943fea59c433ea68d80d9471719a43c3e68
Andrew Smith <848smith@gmail.com>
2020-05-29 18:47:00 +0000

    updated the controller for deleting and editing to say delete and patch instead of post.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -114,7 +114,7 @@ class ApplicationController < Sinatra::Base
     erb :"/pokemon/edit"
   end
   
-  update '/pokemon/:id' do
+  patch '/pokemon/:id' do
     pokemon = Pokemon.find(params[:id])
     if params[:name].empty? || params[:element_type].empty? || params[:cp].empty? || params[:fast_move].empty? || params[:power_move].empty? || params[:attack].empty? || params[:defense].empty? || params[:hp].empty?
       redirect "/pokemon/#{pokemon.id}/edit"
